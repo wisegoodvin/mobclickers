46bedc89065d044cf2cf4420f24e9b14fa0083c4
GoodVin <piven.m@gmail.com>
2017-05-21 21:49:49 +0300

    Первая попытка заменить менеджера

--- nebomobi_clicker.user.js
+++ nebomobi_clicker.user.js
@@ -28,7 +28,7 @@ function cl(sel, timer1, timer2) {
 $(function(){
 
 	// действия в лифте
-	if (/^\/lift/.test(self.location.pathname)) {
+	if (/^\/lift/.test(self.location.pathname) || /liftState/i.test(self.location.href)) {
 		// клик на ссылке "поднять на этаж"
 		if($('a.tdu').length) {
 			$('a.tdu').each(function(){
@@ -45,8 +45,17 @@ $(function(){
 		}
 	}
 
+    // заказ товара
+    if ($(".tower .flst:eq(0)").text().toLowerCase().indexOf("выручку") > -1) cl($(".tower .flst a.tdu:eq(0)"));
+    // выложить товар
+    if ($(".tower .flst:eq(0)").text().toLowerCase().indexOf("выложить") > -1) cl($(".tower .flst a.tdu:eq(0)"));
+    // закупить товар
+    if ($(".tower .flst:eq(0)").text().toLowerCase().indexOf("закупить") > -1) cl($(".tower .flst a.tdu:eq(0)"));
+    // закупка конкретного товара
+    if($(".prd").text().replace(/\s{2,}/g,' ').toLowerCase().indexOf("закупить за") > -1) cl($(".prd .action:last a.tdu:eq(0)"));
+
 	// действия в холле
-	if (/^\/home/.test(self.location.pathname) || /login\/?$/.test(self.location.pathname)) {
+	if (/^\/home/.test(self.location.pathname) || /login\/?$/.test(self.location.pathname) || "/" == self.location.pathname) {
 		// развернуть этажи
 		/*if($('a.nshd').length) {
 			$('a.nshd').each(function(){
@@ -54,22 +63,30 @@ $(function(){
 				if(txt.indexOf("Показать этажи") > -1) cl($(this));
 			});
 		}*/
-		// в холле - заказ товара
-		// в холле - выкладка товара
-		// в холле - забрать бабки за товар
-		// поднимаем людей на лифте (клик на кнопке в холле)
-		if($('a.tdn').has('.amount').length) {
+        $(".tlbr > a").each(function(i){
+            // поднимаем людей на лифте (клик на кнопке в холле)
+            if(/lift\./.test($(this).find("img")[0].src) || /lift_vip\./.test($(this).find("img")[0].src)) cl($(this));
+            // в холле - заказ товара
+            if(/empty\./.test($(this).find("img")[0].src)) cl($(this));
+            // в холле - выкладка товара
+            if(/stocked\./.test($(this).find("img")[0].src)) cl($(this));
+            // в холле - забрать бабки за товар
+            if(/sold\./.test($(this).find("img")[0].src)) cl($(this));
+        });
+		/*if($('a.tdn').has('.amount').length) {
 			cl($('a.tdn').has('.amount'));
-		}
-		// и в самом конце - рефреш через определённое время
-		var t = $(".vs .flbdy .flst .amount").text().split(" ");
-		if(t.length) {
-			var secs = 3;
-			if(t.length == 4) secs = secs + parseInt(t[0], 10) * 60 + parseInt(t[2], 10);
-			else if('м' == t[1].substring(0,1)) secs = secs + parseInt(t[0], 10) * 60;
-			else secs = secs + parseInt(t[0], 10);
-			console.log('Запущен таймер ожидания посетителей на '+secs+' секунд.');
-			setTimeout(function(){ self.location.reload(); }, secs * 1000);
-		}
+		}*/
 	}
+
+	// и в самом конце - рефреш через определённое время
+	/*if($(".vs").text().toLowerCase().replace(/\s/g,'').indexOf("посетителя!") > -1) {
+        var t = $(".vs .flbdy .flst .amount").text().split(" ");
+		var secs = 3;
+		if(t.length == 4) secs = secs + parseInt(t[0], 10) * 60 + parseInt(t[2], 10);
+		else if('м' == t[1].substring(0,1)) secs = secs + parseInt(t[0], 10) * 60;
+		else secs = secs + parseInt(t[0], 10);
+		console.log('Запущен таймер ожидания посетителей на '+secs+' секунд.');
+		setTimeout(function(){ self.location.reload(); }, secs * 1000);
+    // в самом  конце - таймер на рандомный клик
+	} else */cl($(".hdr:eq(0)"), 20000, 30000);
 });
