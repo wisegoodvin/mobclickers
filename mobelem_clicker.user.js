204c828a9005c2108626afe0bd3290fc74358623
GoodVin <piven.m@gmail.com>
2017-05-22 10:27:04 +0300

    Изменён метод клика

--- mobelem_clicker.user.js
+++ mobelem_clicker.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Повелители стихий. Кликер
 // @namespace    https://ok.elem.mobi/
-// @version      0.2.2
+// @version      0.3
 // @description  Проводит дуэли (пока что)
 // @author       GoodVin
 // @match        https://ok.elem.mobi/*
-// @include      https://ok.elem.mobi/*
 // @require      http://code.jquery.com/jquery-3.2.1.slim.min.js
 // @downloadURL  https://github.com/wisegoodvin/mobclickers/raw/master/mobelem_clicker.user.js
 // @updateURL    https://github.com/wisegoodvin/mobclickers/raw/master/mobelem_clicker.user.js
@@ -13,16 +12,16 @@
 // ==/UserScript==
 
 // клик
+var clicktimer = false;
 function cl(sel, timer1, timer2) {
+	if(clicktimer) return false;
 	var clickEvent = document.createEvent ('MouseEvents');
 	clickEvent.initEvent ('click', true, true);
 	if(timer1 === undefined) timer1 = 500;
 	if(timer2 === undefined) timer2 = 750;
-	setTimeout(function(){
-		sel.each(function(){
-			this.dispatchEvent (clickEvent);
-		});
-	}, Math.random() * (timer2 - timer1) + timer1);
+	setTimeout(function(){ (sel instanceof jQuery ? sel[0] : sel).dispatchEvent (clickEvent); }, Math.random() * (timer2 - timer1) + timer1);
+	clicktimer = true;
+	return false;
 }
 
 $(function(){
@@ -51,7 +50,7 @@ $(function(){
 			}
 			if(idx15 >= 0) {
 				console.log('Максимальный урон, наносимый при рейтинге 1.5 будет у пары №'+(idx15+1)+': '+(cards[idx15][2]-cards[idx15][0]));
-				cl($(cards[idx15][3]).find("a:eq(1)"));
+				return cl($(cards[idx15][3]).find("a:eq(1)"));
 			}
 			// ищем карты с рейтингом 1
 			if(idx15 == -1) {
@@ -65,7 +64,7 @@ $(function(){
 				}
 				if(idx1 >= 0) {
 					console.log('Максимальный урон, наносимый при рейтинге 1 будет у пары №'+(idx1+1)+': '+(cards[idx1][2]-cards[idx1][0]));
-					cl($(cards[idx1][3]).find("a:eq(1)"));
+					return cl($(cards[idx1][3]).find("a:eq(1)"));
 				}
 				// ищем карты с рейтинго 0.5 с наименьшим уроном по игроку
 				if(idx1 == -1) {
@@ -80,20 +79,20 @@ $(function(){
 				}
 				if(idx05 >= 0) {
 					console.log('Максимальный урон, наносимый при рейтинге 0.5 будет у пары №'+(idx05+1)+': '+(cards[idx05][2]-cards[idx05][0]));
-					cl($(cards[idx05][3]).find("a:eq(1)"));
+					return cl($(cards[idx05][3]).find("a:eq(1)"));
 				}
 			}
 		}
 		// бой окончен - предложение вступить в дуэль ещё раз
-		if($(".cntr a:not(.orange)").has(".lbl").length && $(".cntr a:not(.orange)").has(".lbl").text().trim().toLowerCase().indexOf("еще") > -1) cl($(".cntr a:not(.orange)").has(".lbl"));
+		if($(".cntr a:not(.orange)").has(".lbl").length && $(".cntr a:not(.orange)").has(".lbl").text().trim().toLowerCase().indexOf("еще") > -1) return cl($(".cntr a:not(.orange)").has(".lbl"));
 		// напасть
-		if($(".cntr a:not(.grey)").has(".lbl").length && $(".cntr a:not(.grey)").has(".lbl").text().trim().toLowerCase().indexOf("напасть") > -1) cl($(".cntr a:not(.grey)").has(".lbl"));
+		if($(".cntr a:not(.grey)").has(".lbl").length && $(".cntr a:not(.grey)").has(".lbl").text().trim().toLowerCase().indexOf("напасть") > -1) return cl($(".cntr a:not(.grey)").has(".lbl"));
 		// смена противника - перезагрузка страницы
 		else if("/duel/" == self.location.pathname) setTimeout(function(){ self.location.reload(); }, Math.random() * (2000 - 1000) + 1000);
 		// перерыв в дуэлях
-		if("/duel/" == self.location.pathname && $(".fttl").length && $(".fttl").text().toLowerCase().indexOf("перерыв") > -1) cl($(".small[onclick]"));
+		if("/duel/" == self.location.pathname && $(".fttl").length && $(".fttl").text().toLowerCase().indexOf("перерыв") > -1) return cl($(".small[onclick]"));
         // Новая дуэль
-        if("Новая дуэль" == $(".cntr .be .lbl").text().trim()) cl($(".cntr .btn"));
+        if("Новая дуэль" == $(".cntr .be .lbl").text().trim()) return cl($(".cntr .btn"));
 	}
 
 	// действия в корне
@@ -109,10 +108,10 @@ $(function(){
 				if(ar[i+1] == 'c') secs += parseInt(ar[i], 10);
 			}
 			console.log('Установлен таймер на '+secs+' секунд до начала дуэлей!');
-			setTimeout(function(){ cl($("#duels_restore_time").closest("a")); }, secs * 1000);
+			setTimeout(function(){ return cl($("#duels_restore_time").closest("a")); }, secs * 1000);
 		}
 		// дуэли активны
-		if($(".bttn.duels").length && $(".bttn.duels").text().toLowerCase().indexOf("дуэли:") > -1) cl($(".bttn.duels"));
+		if($(".bttn.duels").length && $(".bttn.duels").text().toLowerCase().indexOf("дуэли:") > -1) return cl($(".bttn.duels"));
 	}
 
 	// действия на арене
