c27823e0de352364a79b58f558ba4c4b5cb1872e
GoodVin <piven.m@gmail.com>
2017-05-22 17:40:34 +0300

    Update kolhmobi_clicker.user.js

--- kolhmobi_clicker.user.js
+++ kolhmobi_clicker.user.js
@@ -4,7 +4,8 @@
 // @version      1.0
 // @description  Высаживает, поливает, удобряет и продаёт растения
 // @author       GoodVin
-// @match        https://*.kolhoz.mobi/*
+// @match        *://*.kolhoz.mobi/*
+// @match        *://kolhoz.mobi/*
 // @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
 // @downloadURL  https://github.com/wisegoodvin/mobclickers/raw/master/kolhmobi_clicker.user.js
 // @updateURL    https://github.com/wisegoodvin/mobclickers/raw/master/kolhmobi_clicker.user.js
@@ -12,8 +13,9 @@
 // @icon         http://kolhoz.mobi/favicon.ico
 // ==/UserScript==
 
+var clicktimer = false;
 // прочие мелкие функции
-function rand(num1, num2) { return Math.random() * (num2 - num1) + num1; }
+function rand(num1, num2) { return Math.round(Math.random() * (num2 - num1) + num1); }
 function hastxt(sel, txt) { return $(sel).text().toLowerCase().replace(/\s/g,'').indexOf(txt.toLowerCase().replace(/\s/g,'')) > -1; }
 function isUndef(elem) { return typeof elem == "undefined"; }
 function empty(txt) { return (isUndef(txt) || txt === '' || txt === null); }
@@ -30,14 +32,15 @@ function str2secs(str) {
 	return res;
 }
 function go(url, timer1, timer2) {
+	if(clicktimer) return false;
 	if(empty(timer1)) timer1 = 500;
-	if(empty(timer2)) timer2 = 750;
-	setTimeout(function(){ self.location.href = url; }, rand(timer1,timer2));
+	if(empty(timer2)) timer2 = timer1;
+    var time = rand(timer1, timer2);
+	setTimeout(function(){ self.location.href = url; }, time);
 	return false;
 }
 
 // клик
-var clicktimer = false;
 function cl(sel, timer1, timer2) {
 	if(clicktimer) return false;
 	var clickEvent = document.createEvent ('MouseEvents');
@@ -50,6 +53,8 @@ function cl(sel, timer1, timer2) {
 }
 
 $(function(){
+	// сначала добавляем кнопку
+	
 	// действия на главном экране
 	$(".block .ptm > ul > li a").each(function(){
 		// покупку грядки пропускаем
@@ -65,30 +70,32 @@ $(function(){
 		// удобрение (надо выбрать заранее)
 		if(hastxt(this, "применить")) return cl(this);
 	});
+    if(clicktimer) return false;
 
-	// вход в амбар (шанс зайти туда - 10%)
-	if(1 == rand(1, 10)) return go('/warehouse');
-	// действия в амбаре
-	if('/warehouse' == self.location.pathname) {
-		console.log('Продажа из амбара');
-		$(".block a").each(function(){ if(hastxt(this, "продать")) return cl(this); });
-		if(hastxt(".block", "амбар пуст")) return go('/');
-	}
+    // вход в амбар (шанс зайти туда - 10%)
+    if(1 == rand(1, 10)) return go('/warehouse');
+    // действия в амбаре
+    if('/warehouse' == self.location.pathname) {
+        console.log('Продажа из амбара');
+        $(".block a").each(function(){ if(hastxt(this, "продать")) return cl(this); });
+        if(hastxt(".block", "амбар пуст")) return go('/');
+    }
+    if(clicktimer) return false;
 
-	// запуск таймера
-	var txt = $(".ptm > ul > li").text().toLowerCase().replace(/\n/g,' ').replace(/\r/g,'').replace(/\s{1,}/g,' ').trim();
-	if(!empty(txt)) {
-		const timeregex = /\d+\s+(?:ч|м|с)[а-яё\s]+\d+\s(?:ч|м|с)/g;
-		var mintime = null;
-		var idx = 0;
-		while((res = timeregex.exec(txt)) !== null) {
-			if (res.index === timeregex.lastIndex) timeregex.lastIndex++;
-			if(empty(res[0])) continue;
-			var t = str2secs(res[0]);
-			if(empty(mintime) || t < mintime) mintime = t;
-		}
-		mintime += 5;
-		console.log('Никаких действие не сделано - запущен таймер на '+mintime+' сек. до ближайшего действия.');
-		return go("/", mintime * 1000);
-	}
+    // запуск таймера
+    var txt = $(".ptm > ul > li").text().toLowerCase().replace(/\n/g,' ').replace(/\r/g,'').replace(/\s{1,}/g,' ').trim();
+    if(!empty(txt)) {
+        const timeregex = /(\d+\s+(?:ч|м|с)[а-яё\s]+)?\d+\s(?:ч|м|с)/g;
+        var mintime = null;
+        var idx = 0;
+        while((res = timeregex.exec(txt)) !== null) {
+            if (res.index === timeregex.lastIndex) timeregex.lastIndex++;
+            if(empty(res[0])) continue;
+            var t = str2secs(res[0]);
+            if(empty(mintime) || t < mintime) mintime = t;
+        }
+        mintime += 3;
+        console.log('Никаких действие не сделано - запущен таймер на '+mintime+' сек. до ближайшего действия.');
+        return go("/", mintime * 1000);
+    }
 });
