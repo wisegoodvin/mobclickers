0646ed15f7e3e86d18510d7c560c2beaf6526022
GoodVin <piven.m@gmail.com>
2017-05-24 06:48:08 +0300

    Обновление на версию 2.0
    Много мелких изменений + вывод лога о действии.
    Исправлено повторное нажатие на кнопку с ящиком/монетой/плюсом.
    Функции вынесены в отдельный файл.

--- nebomobi_clicker.user.js
+++ nebomobi_clicker.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Небоскребы: онлайн. Кликер
 // @namespace    https://odkl.vnebo.mobi/
-// @version      1.2
+// @version      2.0
 // @description  Поднимает посетителей на лифте, делает операции с этажами
 // @author       GoodVin
 // @match        *://*.vnebo.mobi/*
 // @match        *://vnebo.mobi/*
 // @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @require      https://github.com/wisegoodvin/mobclickers/raw/master/shared_functions.js?v=1
 // @downloadURL  https://github.com/wisegoodvin/mobclickers/raw/master/nebomobi_clicker.user.js
 // @updateURL    https://github.com/wisegoodvin/mobclickers/raw/master/nebomobi_clicker.user.js
 // @icon         https://vnebo.mobi/favicon.ico
@@ -14,66 +15,6 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
-
-// jQuery plugins
-jQuery.expr[':'].text = function(a, i, m) {
-	return jQuery(a).text().replace(/\s/g,'').toLowerCase()
-		.indexOf(m[3].replace(/\s/g,'').toLowerCase()) >= 0;
-};
-
-var clicktimer = false;
-// разные мелкие функции
-function rand(num1, num2) { return Math.round(Math.random() * (num2 - num1) + num1); }
-function hastxt(sel, txt) { return $(sel).text().toLowerCase().replace(/\s/g,'').indexOf(txt.toLowerCase().replace(/\s/g,'')) > -1; }
-function isUndef(elem) { return typeof elem == "undefined"; }
-function empty(txt) { return (isUndef(txt) || txt === '' || txt === null); }
-function str2secs(str) {
-	str = str.toLowerCase();
-	var res = 0;
-	var a = str.split(' ');
-	if(!a.length) return res;
-	for(i=0;i<a.length;i+=2) {
-		try {
-			var l = a[i+1].substring(0,1);
-				 if('ч' == l) res += parseInt(a[i],10) * 3600;
-			else if('м' == l) res += parseInt(a[i],10) * 60;
-			else if('с' == l) res += parseInt(a[i],10);
-		} catch(e) { res += 0; }
-	}
-	return res;
-}
-function go(url, timer1, timer2) {
-	if(clicktimer) return false;
-	if(empty(timer1)) timer1 = 500;
-	if(empty(timer2)) timer2 = timer1;
-    var time = rand(timer1, timer2);
-	setTimeout(function(){ self.location.href = url; }, time);
-	return false;
-}
-
-// клик
-function cl(sel, timer1, timer2) {
-	if(clicktimer) return false;
-	var clickEvent = document.createEvent ('MouseEvents');
-	clickEvent.initEvent ('click', true, true);
-	if(timer1 === undefined) timer1 = 500;
-	if(timer2 === undefined) timer2 = 750;
-	setTimeout(function(){
-		try {
-			(sel instanceof jQuery ? sel[0] : sel).dispatchEvent (clickEvent);
-		} catch(e) {
-			self.location.reload();
-		}
-	}, rand(timer1, timer2));
-	clicktimer = true;
-	return false;
-}
-
-// функция для отключения кликера - прописывается в родное окно
-unsafeWindow.endis = function() {
-	GM_setValue("scriptenabled", !scriptenabled);
-	self.location.reload();
-}
 
 $(function(){
 	scriptenabled = GM_getValue("scriptenabled", true);
@@ -85,7 +26,8 @@ $(function(){
 	if(explvl.length) return cl(explvl);
 
 	// действия с этажами из холла
-	if("/" == self.location.pathname || "/home" == self.location.pathname || /login\/?$/i.test(self.location.pathname)) {
+	if(("/" == self.location.pathname && !/:floorPanel:/.test(self.location.href)) || "/home" == self.location.pathname || /login\/?$/i.test(self.location.pathname)) {
+        	console.log("Действия на главном экране");
 		// есть какие-то действия
 		var flrs = $(".tlbr a[href*='floor']:text(\()");
 		if(flrs.length) return cl(flrs);
@@ -95,20 +37,34 @@ $(function(){
 	}
 
 	// действия с этажами
-	if(/\/floors\//i.test(self.location.pathname) && $(".tower a.tdu").length) return cl($(".tower a.tdu")[0]);
+	if((/\/floors\//i.test(self.location.pathname) || /:floorPanel:/.test(self.location.href)) && $(".tower a.tdu:notext(биржа)").length) {
+		console.log("Действия с этажами");
+		return cl($(".tower a.tdu:notext(биржа)")[0]);
+	}
 
 	// действия с этажом
 	if(/\/floor\//i.test(self.location.pathname)) {
+        	console.log("Действия на этажаме");
 		// закупки
-		if($(".prd a.tdu:text(купить)").length) return cl($(".prd a.tdu:text(купить):last"));
+		if($(".prd a.tdu:text(купить)").length) {
+			console.log("Закупка товара");
+			return cl($(".prd a.tdu:text(купить):last"));
+		}
 	}
 
 	// у лифта есть ожидающие
 	if(/^\/lift/i.test(self.location.pathname)) {
-		// закупки
+        	console.log("Действия внутри лифта");
+		// в лифте
 		if($(".lift").length) {
-			if($(".lift a.tdu:text(поднять)").length) return cl($(".lift a.tdu:text(поднять)"));
-			if($(".lift a.tdu:text(чаевые)").length) return cl($(".lift a.tdu:text(чаевые)"));
+			if($(".lift a.tdu:text(поднять)").length) {
+                console.log("Пиднимаю");
+                return cl($(".lift a.tdu:text(поднять)"));
+            }
+            if($(".lift a.tdu:text(чаевые)").length) {
+                console.log("Чаевые");
+                return cl($(".lift a.tdu:text(чаевые)"));
+            }
 		}
 	}
 
